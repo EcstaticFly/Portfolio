c07cb5e2f0b9f174f559f9aaa8dbc5f75d8902a4
Suyash Pandey <suyashpandey607@gmail.com>
2025-03-09 13:40:47 +0530

    feat: add RecentProjects component to showcase selected projects; update FloatingNav with Resume link

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
+import RecentProjects from "../components/RecentProjects";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProjects/>
       </div>
     </main>
   );
