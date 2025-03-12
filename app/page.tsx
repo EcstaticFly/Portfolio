64ed717318632edde93139c20ce6e019f869e187
Suyash Pandey <suyashpandey607@gmail.com>
2025-03-12 18:36:49 +0530

    refactor: rename CustomCursor to CosmicCursor for consistency

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import Approach from "@/components/Approach";
 import Footer from "@/components/Footer";
 import dynamic from "next/dynamic";
 
-const CustomCursor = dynamic(() => import("@/components/Cursor"), {
+const CosmicCursor = dynamic(() => import("@/components/Cursor"), {
   ssr: true,
 });
 
@@ -21,7 +21,7 @@ const Home = () => {
   if (!mounted) return <div className="h-screen bg-black-100" />;
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
-      {<CustomCursor />}
+      {<CosmicCursor />}
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
