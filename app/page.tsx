5910de70927a66f366e817162712969e6bc455c3
Suyash Pandey <suyashpandey607@gmail.com>
2025-03-12 09:43:44 +0530

    feat: add custom animated cursor component for enhanced interactivity

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,18 @@ import Clients from "@/components/Clients";
 import RecentActivities from "@/components/RecentActivities";
 import Approach from "@/components/Approach";
 import Footer from "@/components/Footer";
+import dynamic from "next/dynamic";
+
+const CustomCursor = dynamic(() => import("@/components/Cursor"), {
+  ssr: true,
+});
 
 const Home = () => {
   const mounted = useMounted();
   if (!mounted) return <div className="h-screen bg-black-100" />;
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
+      {<CustomCursor />}
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
