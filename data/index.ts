22728db892d8f2b37f2ef9d44e1be8d086dbbcdc
Suyash Pandey <suyashpandey607@gmail.com>
2025-03-14 19:01:08 +0530

    refactor: improve grid item title for clarity and impact

--- data/index.ts
+++ data/index.ts
@@ -20,7 +20,7 @@ export const gridItems = [
   {
     id: 2,
     title:
-      "Adaptable and committed to delivering excellence, regardless of time zones.",
+      "I aspire to deliver solutions beyond time zones and borders.",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
