11bb7d3695b2a7a6dc7ee21d42435de83e77cf39
Suyash Pandey <suyashpandey607@gmail.com>
2025-04-24 22:21:29 +0530

    fix: comment out unused FloatingDots import and component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import { ArrowUpRightIcon } from "lucide-react";
 import MagicButton from "./MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
-import FloatingDots from "./ui/FloatingDots";
+// import FloatingDots from "./ui/FloatingDots";
 
 const Hero = () => {
   return (
@@ -20,7 +20,7 @@ const Hero = () => {
       </div>
 
       <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
-        <FloatingDots />
+        {/* <FloatingDots /> */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
       <div className="flex justify-center relative my-20 z-10">
